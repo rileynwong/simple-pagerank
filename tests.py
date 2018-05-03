561255632f599cb3be2bcc27dd672cc13847a93e
Riley Wong <nancypwong@gmail.com>
2018-05-03 15:02:05 -0400

    Lol change graph implementation to include inbound edges

--- tests.py
+++ tests.py
@@ -4,42 +4,58 @@ Use with nosetests.
 """
 
 from .graph import Graph
+from .graph import Node
 
 from nose.tools import assert_equals
 
+### Test graph implementation
 def test_graph_init():
     # Test that initializing graph to a given dictionary works
-    graph_dict = {
-            'a': ['b'],
-            'b': ['c'],
-            'c': []
+    node_dict = {
+            'a': Node(),
+            'b': Node(),
+            'c': Node()
         }
 
-    test_graph = Graph(graph_dict)
+    test_graph = Graph(node_dict)
 
-    assert_equals(test_graph.graph, graph_dict)
+    assert_equals(test_graph.nodes, node_dict)
 
 
 def test_graph_add_node():
     # Test that adding a node to an empty graph works
     test_graph = Graph()
-    test_graph.add_node('a')
+    node_a = Node()
 
-    assert_equals(test_graph.graph, {'a': []})
+    test_graph.add_node('a', node_a)
 
-    test_graph.add_node('b', ['a'])
-    assert_equals(test_graph.graph, {'a': [], 'b': ['a']})
+    assert_equals(test_graph.nodes['a'].inbound, [])
+    assert_equals(test_graph.nodes['a'].outbound, [])
+
+    node_b = Node(inbound=[], outbound=[node_a])
+    test_graph.add_node('b', node_b)
+
+    assert_equals(test_graph.nodes['b'].inbound, [])
+    assert_equals(test_graph.nodes['b'].outbound, [node_a])
+
+    assert len(test_graph.nodes) == 2
 
 
 def test_graph_add_edge():
     # Test adding an edge to the graph
     test_graph = Graph()
-    test_graph.add_node('a')
-    test_graph.add_node('b')
+    node_a = Node()
+    node_b = Node()
 
-    assert_equals(test_graph.graph, {'a': [], 'b': []})
+    test_graph.add_node('a', node_a)
+    test_graph.add_node('b', node_b)
+
+    assert_equals(test_graph.nodes, {'a': node_a, 'b': node_b})
 
     test_graph.add_edge('a', 'b')
 
-    assert_equals(test_graph.graph, {'a': ['b'], 'b': []})
+    assert_equals(node_a.outbound, [node_b])
+    assert_equals(node_b.inbound, [node_a])
 
+
+### Test PageRank
